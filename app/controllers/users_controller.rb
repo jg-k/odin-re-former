9294ef887339448fd256ce92ada6eaa8363e3b22
Jean-Gregoire Kherian <jgkherian@gmail.com>
2024-10-08 00:00:38 +0100

    Setup manual form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def new
+    
+  end
+
+  def create 
+    puts "helloooooooo"
+    puts params
+    @user = User.new(user_params)
+    p @user.valid?
+    p @user.errors.messages
+    
+
+    if @user.save 
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  def user_params 
+    params.require(:user).permit(:email, :password)
+
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  validates :email, :password, presence: true
+
+end
