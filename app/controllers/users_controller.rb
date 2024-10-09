7cd480b6b1ed2978ce805bfe2a99b28da01a3af2
Jean-Gregoire Kherian <jgkherian@gmail.com>
2024-10-09 09:03:51 +0100

    Cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,11 @@ class UsersController < ApplicationController
   end
 
   def create 
-    puts "helloooooooo"
-    puts params
     @user = User.new(user_params)
+
     p @user.valid?
     p @user.errors.messages
     
-
     if @user.save 
       redirect_to new_user_path
     else
@@ -21,6 +19,5 @@ class UsersController < ApplicationController
 
   def user_params 
     params.require(:user).permit(:email, :password)
-
   end
 end
